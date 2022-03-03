76b6ba6d14debf45e56f997a939ec3b4af5fd86a
Anurag Mahendru <iamanuragmahendru@gmail.com>
2022-03-03 20:11:39 +0530

    Completed Cart and Add/Remove Functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,11 +8,11 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Reat Meals</h1>
-        <HeaderCartButton />
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A table full of delicious food" />
+        <img src={mealsImage} alt="A Table full of delicious food!" />
       </div>
     </Fragment>
   );
